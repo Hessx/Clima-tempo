5ee678227e3a460ed6cc96b3760f878260fdac88
Hessx <vitorhesss@gmail.com>
2025-01-14 18:53:40 -0300

    Styles e ícones

--- src/App.js
+++ src/App.js
@@ -115,7 +115,7 @@ const App = () => {
           fontSize: "16px",
           borderRadius: "5px",
           border: "none",
-          backgroundColor: "#007BFF",
+          backgroundColor: "#0054AE",
           color: "#e6e5e5",
           cursor: "pointer",
         }}
@@ -129,7 +129,7 @@ const App = () => {
           fontSize: "16px",
           borderRadius: "5px",
           border: "none",
-          backgroundColor: "#FF4D4D",
+          backgroundColor: "#AE0303 ",
           color: "#e6e5e5",
           cursor: "pointer",
         }}
@@ -152,16 +152,71 @@ const App = () => {
             {weather.name}, {weather.sys.country}
           </h2>
           <p>
-            <strong>Temperatura:</strong> {weather.main.temp}°C
+            <strong>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="16"
+                fill="currentColor"
+                class="bi bi-thermometer-low"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V9.5a.5.5 0 0 1 1 0v1.585a1.5 1.5 0 0 1 1 1.415" />
+                <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1" />
+              </svg>
+              Temperatura:
+            </strong>{" "}
+            {weather.main.temp}°C
           </p>
           <p>
-            <strong>Descrição:</strong> {weather.weather[0].description}
+            <strong>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="16"
+                fill="currentColor"
+                class="bi bi-card-heading"
+                viewBox="0 0 16 16"
+              >
+                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z" />
+                <path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z" />
+              </svg>
+              Descrição:
+            </strong>{" "}
+            {weather.weather[0].description}
           </p>
           <p>
-            <strong>Umidade:</strong> {weather.main.humidity}%
+            <strong>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="16"
+                fill="currentColor"
+                class="bi bi-moisture"
+                viewBox="0 0  16"
+              >
+                <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267" />
+              </svg>
+              Umidade:
+            </strong>{" "}
+            {weather.main.humidity}%
           </p>
           <p>
-            <strong>Vento:</strong> {weather.wind.speed} km/h
+            <strong>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="16"
+                fill="currentColor"
+                class="bi bi-wind"
+                viewBox="0 0 16 16"
+              >
+                <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5" />
+              </svg>
+              Vento:
+            </strong>{" "}
+            {weather.wind.speed} km/h
           </p>
         </div>
       )}
